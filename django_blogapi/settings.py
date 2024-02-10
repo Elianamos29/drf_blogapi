f78c68d3430bcbd03ed6694a4b883edd18b9cf30
Elianamos <elianamos29@gmail.com>
2024-02-10 10:40:21 +0300

    add login and logout endpoints with dj-rest-auth

--- django_blogapi/settings.py
+++ django_blogapi/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'corsheaders',
     'rest_framework.authtoken',
+    'dj_rest_auth',
     # local
     'accounts.apps.AccountsConfig',
     'posts.apps.PostsConfig',
