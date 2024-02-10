480b4067accff867bd9839527f83361793934004
Elianamos <elianamos29@gmail.com>
2024-02-10 10:24:21 +0300

    configure default authentication classes

--- django_blogapi/settings.py
+++ django_blogapi/settings.py
@@ -136,6 +136,10 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ],
 }
 
 CORS_ORIGIN_WHITELIST = (
